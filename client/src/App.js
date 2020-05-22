2af72fb6b41c65ba8012191c844f4374ef5954f1
Michael Orwin <michaelxorwin@gmail.com>
2020-05-21 21:22:43 -0400

    construction sign

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+        <h1>Michael Orwin</h1>
+        <h2>Software Developer</h2>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h4>This website is currently under development</h4>
       </header>
     </div>
   );
