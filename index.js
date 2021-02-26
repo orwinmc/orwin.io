f43df2dc8ce6cf9f9f4d7a471f507c0d64147ab0
Michael Orwin <michaelxorwin@gmail.com>
2021-02-26 15:39:20 -0600

    Moving from Heroku to Vercel (#3)
    * moving from heroku to vercel
    
    * test unsafe-perm flag
    
    * testing
    
    * testing more
    
    * testing more 2

--- index.js
+++ index.js
@@ -3,22 +3,20 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+const port = process.env.PORT || 3000;
 
-// Put all API endpoints under '/api'
-app.get('/api/test', (req, res) => {
-  // Return them as json
-  res.json({test:3});
+// API
+app.get('/api', (req, res) => {
+  res.json({"status":"OK"});
 });
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'web/build')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'web/build', 'index.html'));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`Listening on ${port}`);
+app.listen(port, () => console.log(`Listening on ${port}`));
