e274c38d0757275f145ef60efe4a6c7aa85b853c
Michael Orwin <michaelxorwin@gmail.com>
2021-02-26 01:21:29 -0600

    moving from heroku to vercel

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.get('/api', (req, res) => {
+  res.json({status:"OK"});
+});
+
+app.listen(port, () => console.log(`Listening on ${port}`));
