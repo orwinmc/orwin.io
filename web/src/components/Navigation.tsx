d8af619a499911b0b36c8346aca5b1f38242d847
Michael Orwin <michaelxorwin@gmail.com>
2021-03-02 12:17:09 -0600

    fix link typo

--- web/src/components/Navigation.tsx
+++ web/src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/Link'
+import Link from 'next/link'
 import React from 'react'
 
 import styled from 'styled-components'
--- web/src/pages/[page].tsx
+++ web/src/pages/[page].tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import Link from 'next/Link'
 
 function About() {
     const router = useRouter()
