c399e5c99d835d5abbdaedfc562b8e2e53871627
Michael Orwin <michaelxorwin@gmail.com>
2021-03-09 01:14:14 -0600

    modify next.config.js to match so

--- web/next.config.js
+++ web/next.config.js
@@ -3,7 +3,9 @@ const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    sw: '/sw.js',
     runtimeCaching,
   },
 })
